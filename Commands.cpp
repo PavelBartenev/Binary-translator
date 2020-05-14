cc86beb819392d1755d9f7fe0a08deb73c4d6f40
PavelBartenev <56157750+PavelBartenev@users.noreply.github.com>
2020-05-15 02:20:23 +0300

    Update Commands.cpp

--- Commands.cpp
+++ Commands.cpp
@@ -1,4 +1,4 @@
-﻿enum Commands
+enum Commands
 {
 	CMD_END,
 	CMD_PUSH,
@@ -41,3 +41,41 @@
 	CMD_SET_LINE,
 	CMD_CREATE_WINDOW,
 };
+
+
+int commands_size_in_bytes[30] = {};
+
+int fill_sizes()
+{
+	commands_size_in_bytes[CMD_END] = 12;
+
+	commands_size_in_bytes[CMD_PUSH] = 2; //or 5
+
+	commands_size_in_bytes[CMD_ADD] = 6;
+
+	commands_size_in_bytes[CMD_SUB] = 6;
+
+	commands_size_in_bytes[CMD_MUL] = 7;
+
+	commands_size_in_bytes[CMD_DIV] = 12;
+
+	commands_size_in_bytes[CMD_PUSH_ax] = 1;
+	
+	commands_size_in_bytes[CMD_PUSH_bx] = 1;
+
+	commands_size_in_bytes[CMD_PUSH_cx] = 1;
+
+	commands_size_in_bytes[CMD_PUSH_dx] = 1;
+
+	commands_size_in_bytes[CMD_POP_ax] = 1;
+
+	commands_size_in_bytes[CMD_POP_bx] = 1;
+
+	commands_size_in_bytes[CMD_POP_cx] = 1;
+
+	commands_size_in_bytes[CMD_POP_dx] = 1;
+
+	commands_size_in_bytes[CMD_JMP] = 5;
+
+	return 0;
+}
